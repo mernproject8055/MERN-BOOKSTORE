6e3192ce62b9ec9dedf70e6d2e8944edd27278f0
unknown <chmcs2026.shivam.vishwakarma@gmail.com>
2026-02-20 08:29:29 +0530

    initial commit

---
+++ backend/src/routes/bookRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createBook, deleteBook, getAllBooks, getBookById, updateBook } from "../controllers/bookControllers.js";
+const router=express.Router()
+router.get("/",getAllBooks)
+router.get("/:id",getBookById)
+router.post("/",createBook)
+router.put("/:id",updateBook)
+router.delete("/:id",deleteBook)
+export default router
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router'
+import { PlusIcon } from 'lucide-react'
+const Navbar = () =>{
+    return (
+        <header className='bg-amber-300 border-b border-base-constant/10'>
+            <div className='ms-auto max-w-6xl p-4'>
+                <div className='flex items-center justify-between'>
+                    <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>BOOKSTRORE</h1>
+                    <div className='flex items-center gap-4'>
+                        <Link to={"/create"} className="btn btn-primary">
+                        <PlusIcon className='size-5'/><span>New Book</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Navbar
